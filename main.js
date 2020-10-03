e9019a46ed95b197982266666ebaa85637d9c5c9
ethan <izawaryosuke0101@gmail.com>
2020-10-03 12:11:56 +0900

    canpushenterTask

--- main.js
+++ main.js
@@ -2,13 +2,27 @@
 
 {
   //タスクリスト
-  document.querySelector("button").addEventListener("click", () => {
+  // document.querySelector("button").addEventListener("click", () => {
+  //   const li = document.createElement("li");
+  //   const text = document.querySelector("input");
+  //   li.textContent = text.value;
+  //   document.querySelector("ul").appendChild(li);
+  //   text.value = "";
+  //   text.focus();
+  // });
+}
+
+{
+  //タスクリスト　エンターキーで送信
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("submit");
     const li = document.createElement("li");
     const text = document.querySelector("input");
     li.textContent = text.value;
     document.querySelector("ul").appendChild(li);
-
     text.value = "";
     text.focus();
   });
 }
+
